aefe5a5aabf95d7b97a00de65ea37037882611b3
aviadgawl <aviadgawl@gmail.com>
2025-11-06 09:25:56 +0200

    version 2.0.4

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
     <main className="App">
       <header>
         <GameTabs disabledTabIndexes={disabledTabIndex} displayMode={currentDisplay} onModeClick={handleDisplaySelect} />
-        <div className="Version-number">2.0.3</div>
+        <div className="Version-number">2.0.4</div>
       </header>
       {currentDisplay === 0 && <Login />}
       {currentDisplay === 1 && <DeckBuilder />}
